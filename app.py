6e5aec339f67fd242f7ebfe4b442c9db9e4fb961
nannapravalika <nannapravalika566@gmail.com>
2024-02-05 00:20:38 +0530

    updated admin panel

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import os
-from flask import Flask, render_template, request,redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, send_file
+from io import BytesIO
+import tempfile
+import csv
+import os 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,7 +10,7 @@ app.secret_key = 'treyas19012024'
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # postgres://treyas_user:MiO3siLFd4jS5CUTjE79840SZYszUfwZ@dpg-cmv6jc0l5elc73eckmlg-a.oregon-postgres.render.com/treyas
+    # postgresql://treyas_user:MiO3siLFd4jS5CUTjE79840SZYszUfwZ@dpg-cmv6jc0l5elc73eckmlg-a.oregon-postgres.render.com/treyas
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app.config.from_object(Config)
@@ -87,6 +90,10 @@ def Product_management_and_analytics():
     
     return render_template('Product_management_and_analytics.html')
 
+@app.route('/enroll')
+def enroll():
+    return render_template('enroll_now.html')
+
 @app.route('/Agile methodologies training')
 def Agile_methodologies_training():
     
@@ -104,6 +111,36 @@ def submit():
         # Handle other HTTP methods if needed
         flash('Invalid form submission.')
         return redirect(url_for('index'))
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        if username == 'admin' and password == 'admin_password':
+            form_data = FormData.query.all()
+
+            # Create a temporary file
+            temp_file, temp_filename = tempfile.mkstemp(suffix='.csv')
+
+            with os.fdopen(temp_file, 'w', newline='', encoding='utf-8') as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+                # Write header
+                csv_writer.writerow(['ID', 'Name', 'Phone Number', 'Email'])
+
+                # Write data rows
+                for data in form_data:
+                    csv_writer.writerow([data.id, data.name, data.phone_number, data.email])
+
+            # Send CSV file for download
+            return send_file(temp_filename, mimetype='text/csv', as_attachment=True, download_name='form_data.csv')
+
+        else:
+            flash('Invalid credentials. Please try again.')
+
+    return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
